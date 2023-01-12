e75615ae76c5a3ed32630d952727f43537dc1b33
elakkiyaa20 <elakkiyaa.ms@incedoinc.com>
2023-01-12 11:21:57 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+// const fs = require('fs'); const parse = require('StudentData'); 
+// function readCsv(filePath) {  return new Promise((resolve, reject) => {    
+//     fs.readFile(filePath, (err, data) => {      
+//         if (err) {        reject(err);      }      
+//         parse(data, { columns: true }, (err, rows) => {       
+//              if (err) {          reject(err);        }       
+//               resolve(rows);      
+//             });    
+//         });  
+//     }); 
+// }
+
+const csvParser = require('csv-parser');
+const fs = require('fs');
+const filepath = './StudentData.csv';
+fs.createReadStream(filepath)
+    .on('error', () => {
+        
+    })
+
+    .pipe(csvParser())
+    .on('data', (row) => {
+        console.log(row);
+        
+    })
+
+    .on('end', () => {
+        
+    })
+
+
